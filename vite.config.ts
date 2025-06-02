75a183b8807d2993c538faa0fb8bdcd345a16db2
faniadifrihandoko1 <faniadifrihandoko01@gmail.com>
2025-06-02 12:26:21 +0700

    fix: fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import react from "@vitejs/plugin-react";
-import path from "path";
 import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
